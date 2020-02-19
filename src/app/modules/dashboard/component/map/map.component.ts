e1e058825a57cd05d6c5aecec35979cde32f4fd1
Dian Ramdhani Lubis <dian.ramdhani@gmail.com>
2020-02-19 10:05:55 +0700

    update map: enable click event

--- src/app/modules/dashboard/component/map/map.component.ts
+++ src/app/modules/dashboard/component/map/map.component.ts
@@ -11,7 +11,8 @@ import { Trip } from '@data/scheme/trip';
 export class MapComponent implements OnInit {
   @ViewChild('map', { static: true }) mapEl: ElementRef;
   map: google.maps.Map;
-  trips: Trip[];
+  trips: Trip[] = [];
+  protected infoWindowClick = new google.maps.InfoWindow();
 
   constructor(
     private tripService: TripService
@@ -26,53 +27,58 @@ export class MapComponent implements OnInit {
     this.tripService.getTripByTrack(1)
       .subscribe(res => {
         this.trips = res;
-        this.trips.forEach(trip => {
-          // infoWindow
-          const infoWindowHover = new google.maps.InfoWindow(),
-            // infoWindowClick = new google.maps.InfoWindow(),
-            setContentInfoWindow = (
-              lat: number,
-              lng: number,
-              _infoWindow: google.maps.InfoWindow
-            ) => {
-              const content = `
-                <ul>
-                    <li>
-                        <strong>IRI: </strong>${trip.iri.iriScore}
-                    </li>
-                    <li>
-                        <strong>Latitude: </strong>${lat}
-                    </li>
-                    <li>
-                        <strong>Longitude: </strong>${lng}
-                    </li>
-                </ul>
-                `;
-              _infoWindow.setContent(content);
-              _infoWindow.setPosition({ lat, lng });
-              _infoWindow.open(this.map);
-            };
-
-          // polyline
-          const polyline = new google.maps.Polyline({
-            strokeColor: '#FF0000',
-            path: [
-              new google.maps.LatLng(trip.startLatitude, trip.startLongitude),
-              new google.maps.LatLng(trip.stopLatitude, trip.stopLongitude),
-            ]
-          });
-          polyline.addListener('mouseover', (e: google.maps.MouseEvent) => {
-            setContentInfoWindow(e.latLng.lat(), e.latLng.lng(), infoWindowHover);
-          });
-          polyline.addListener('mouseout', () => {
-            infoWindowHover.close();
-          });
-          // polyline.addListener('click', (e: google.maps.MouseEvent) => {
-          //   infoWindowClick.close();
-          //   setContentInfoWindow(e.latLng.lat(), e.latLng.lng(), infoWindowClick);
-          // });
-          polyline.setMap(this.map);
-        });
+        if (this.trips.length) {
+          for (const i in this.trips) {
+            this.drawer(this.trips[i]);
+          }
+        }
       });
+  }
+
+  protected drawer(trip: Trip) {
+    // drawer infoWindow
+    const infoWindowHover = new google.maps.InfoWindow(),
+      setContentInfoWindow = (
+        lat: number,
+        lng: number,
+        _infoWindow: google.maps.InfoWindow
+      ) => {
+        const content = `
+        <ul>
+            <li>
+                <strong>IRI: </strong>${trip.iri.iriScore}
+            </li>
+            <li>
+                <strong>Latitude: </strong>${lat}
+            </li>
+            <li>
+                <strong>Longitude: </strong>${lng}
+            </li>
+        </ul>
+        `;
+        _infoWindow.setContent(content);
+        _infoWindow.setPosition({ lat, lng });
+        _infoWindow.open(this.map);
+      };
+
+    // drawer polyline
+    const polyline = new google.maps.Polyline({
+      strokeColor: 'blue',
+      path: [
+        new google.maps.LatLng(trip.startLatitude, trip.startLongitude),
+        new google.maps.LatLng(trip.stopLatitude, trip.stopLongitude),
+      ]
+    });
+    polyline.addListener('mouseover', (e: google.maps.MouseEvent) => {
+      setContentInfoWindow(e.latLng.lat(), e.latLng.lng(), infoWindowHover);
+    });
+    polyline.addListener('mouseout', () => {
+      infoWindowHover.close();
+    });
+    polyline.addListener('click', (e: google.maps.MouseEvent) => {
+      this.infoWindowClick.close();
+      setContentInfoWindow(e.latLng.lat(), e.latLng.lng(), this.infoWindowClick);
+    });
+    polyline.setMap(this.map);
   }
 }
